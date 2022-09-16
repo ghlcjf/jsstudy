4ede66a17318659f69af50961dd0f740849397a7
ghlcjf <ghlcjf9676@gmail.com>
2022-09-16 17:58:18 +0900

    js220916

---
+++ ex01.js
@@ -0,0 +1,24 @@
+console.log('배부릅니다. ');
+
+
+// 변수 
+
+// type (동적 자료형) : var -> 호이스팅 
+// 호이스팅을 방지하기 위한 타입 -> let
+
+var a = 10;
+var b = "안녕하세요";
+var c = 10.5;
+
+let num = 10;
+
+console.log("숫자 출력 : "+ num);
+// var num = 10; var 타입은 선언을 나중에 해도 오류가 안 남.
+
+//----------------------
+var a = 50;
+// let num = 20;
+num = 20;
+console.log("숫자 출력 : "+ num);
+
+let d=1, e=2, f=3;
